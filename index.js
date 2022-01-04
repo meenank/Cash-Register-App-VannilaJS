fcf33c30d307c4cd50a3fc3bb61623773339fd13
Varun Gungi <54449583+meenank@users.noreply.github.com>
2022-01-04 12:21:56 +0530

    updaed arror handling

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
 const billAmountVar = document.querySelector("#bill-amount");
 const cashGivenVar = document.querySelector("#cash-given");
 const buttonVar = document.querySelector("#submitbtn");
+
+const errorPara = document.querySelector("#err");
+
+const billError = "*Enter valid bill amount greater than 0";
+const cashError = "*Enter valid cash/amount given which is greater or equal to bill amount";
+
+buttonVar.addEventListener("click", () => Validateinput(billAmountVar, cashGivenVar));
+
+function showMsg(msg) {
+
+    errorPara.style.display = "block";
+    errorPara.style.color = "red";
+    errorPara.innerText = msg;
+
+}
+
+function calcChange(diff) {
+
+
+}
+
+function Validateinput(BA, CG) {
+    errorPara.style.display = "none";
+    if (BA.value > 0) {
+        if (CG.value < BA.value) {
+            showMsg(cashError);
+        } else {
+            const ChangeToGive = CG.value - BA.value;
+            calcChange(ChangeToGive);
+        }
+    } else {
+        showMsg(billError);
+    }
+}
