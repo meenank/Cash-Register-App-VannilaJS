247a516d2751b5fd0d26a84b055a490629d0f6d3
Varun Gungi <54449583+meenank@users.noreply.github.com>
2022-01-04 17:02:26 +0530

    completed the website, added relevant styling

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const billAmountVar = document.querySelector("#bill-amount");
 const cashGivenVar = document.querySelector("#cash-given");
 const buttonVar = document.querySelector("#submitbtn");
-
+const noOfNotes = document.querySelectorAll(".changeDis");
 const errorPara = document.querySelector("#err");
 
 const billError = "*Enter valid bill amount greater than 0";
 const cashError = "*Enter valid cash/amount given which is greater or equal to bill amount";
+
+const denos = [2000, 500, 100, 20, 10, 5, 1];
 
 buttonVar.addEventListener("click", () => Validateinput(billAmountVar, cashGivenVar));
 
@@ -17,21 +19,29 @@ function showMsg(msg) {
 
 }
 
-function calcChange(diff) {
-
-
-}
-
 function Validateinput(BA, CG) {
     errorPara.style.display = "none";
     if (BA.value > 0) {
-        if (CG.value < BA.value) {
+        if (CG.value <= BA.value) {
             showMsg(cashError);
         } else {
-            const ChangeToGive = CG.value - BA.value;
+            const ChangeToGive = (CG.value - BA.value);
             calcChange(ChangeToGive);
         }
     } else {
         showMsg(billError);
+    }
+}
+
+
+function calcChange(diff) {
+
+    for (var i = 0; i < denos.length; i++) {
+
+        var notes = Math.trunc(diff / denos[i]);
+
+        diff %= denos[i];
+
+        noOfNotes[i].innerText = notes;
     }
 }
